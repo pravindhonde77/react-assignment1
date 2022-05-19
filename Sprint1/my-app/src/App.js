7933a7db26b5bddbd9ff4edde4e11f48f8ca2ad3
pravin dhonde <pravindhonde77@gmail.com>
2022-05-19 18:42:58 +0530

    assignment1

---
+++ Sprint1/my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Mobile from './Day1/components/Mobile';
+
+function App() {
+  return (
+    <div className="App">
+     <Mobile/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Sprint1/my-app/src/Day1/components/Mobile.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Mobile = () => {
+  return (
+    <div>
+        <h1>Mobile Operating System</h1>
+        <li>Android</li>
+        <li>Blackberry</li>
+        <li>iphone</li>
+        <li>Windows Phone</li>
+
+        <h1>Mobile Manufacturers</h1>
+        <li id='sq' >Samsung</li>
+        <li id='sq' >HTC</li>
+        <li>Micromax</li>
+        <li id='disc' >Apple</li>
+      
+    </div>
+  )
+}
+
+export default Mobile
