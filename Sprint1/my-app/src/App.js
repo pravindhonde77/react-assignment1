5121020636b3c5bd6aaf04c050c86daa07bbf928
pravin dhonde <pravindhonde77@gmail.com>
2022-05-21 00:14:22 +0530

    navbar

--- Sprint1/my-app/src/App.js
+++ Sprint1/my-app/src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import Mobile from './Day1/components/Mobile';
+
+// import './App.css';
+// import Mobile from './Day1/components/Mobile';
+import Navbar from './Day1/components/Navbar';
 
 function App() {
   return (
     <div className="App">
-     <Mobile/>
+     {/* <Mobile/> */}
+
+      <Navbar/>
     </div>
   );
 }
